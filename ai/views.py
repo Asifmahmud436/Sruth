e9f10c761dd26c9da5095ffa6c2c086315e7998a
Zettex <safaandsafa4@gmail.com>
2025-02-01 11:22:35 +0600

    the llm text is displayable on the monitor

--- ai/views.py
+++ ai/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+from dotenv import load_dotenv
+from groq import Groq
 
-# Create your views here.
+MY_KEY = os.getenv('SRUTH_KEY')
+
+
+client = Groq(
+    api_key=MY_KEY
+)
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role": "user",
+            "content": "Can you see this?"
+        },
+        
+    ],
+    temperature=1,
+    # max_completion_tokens=1024,
+    # top_p=1,
+    # stream=True,
+    # stop=None,
+)
+
+def home(request):
+    return HttpResponse(response.choices[0].message.content)
